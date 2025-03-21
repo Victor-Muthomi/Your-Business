4fd1ee5db90bd71f9c8eb8d142c19a69ef85a962
E221/26496/2023 NAOM CHEPKEMOI NYONGA <26496@uoemdomain.embuni.ac.ke>
2025-03-21 10:32:03 +0300

    Had to deploy it

---
+++ officewithnoroof.js
@@ -0,0 +1,37 @@
+
+(function(){
+    emailjs.init("u1XLHV7CRKz9x4IFJ");  // Replace with your actual public key
+})();
+
+document.getElementById("contact-form").addEventListener("submit", function(event){
+event.preventDefault();
+
+let form = document.getElementById("contact-form");
+
+let params = {
+    receiver: document.getElementById("receiver").value,
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value
+};
+
+emailjs.send("service_8496cer", "template_jg4tvqi", params)
+.then(response => {
+    Swal.fire({
+        title: "Office with no roof",
+        text: "Mail sent successfully!",
+        icon: "success",
+        confirmButtonText: "OK"
+    });
+       form.reset();  // Clear the form inputs after sending
+})
+.catch(error => {
+    Swal.fire({
+        title: "Office with no roof",
+        text: "Failed to send message. Please try again.",
+        icon: "error",
+        confirmButtonText: "OK"
+    });
+});
+});
